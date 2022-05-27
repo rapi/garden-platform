59aec7384e9d6bdb323a63b34f2ea39c40e08da7
Vasile Grinciuc <raptordias@gmail.com>
2022-05-27 17:53:44 +0300

    Add new configuration

--- src/main.cpp
+++ src/main.cpp
@@ -9,19 +9,23 @@
 #include <ArduinoOTA.h>
 #include <EEPROM.h>
 
+// const char *ssid = "MOLDTELECOM_F47";
+// const char *password = "3043501914";
 
-const char* ssid = "MOLDTELECOM_F47";
-const char* password = "3043501914";
-// const char* ssid = "HUAWEI-aah9";
-// const char* password = "4gd19aib";
-int PIN_COUNT=15;
-int TIME_PER_ZONE=20;
-bool IS_STOP_WATERING=false;
-bool IS_STARTED_WATERING=false;
+const char *HOSTssid = "GARDEN-AP";
+const char *HOSTpassword = "62636263";
+
+String ssid = "HUAWEI-aah9";  // OK
+String password = "4gd19aib"; // OK
+
+int PIN_COUNT = 16;
+int TIME_PER_ZONE = 20;
+bool IS_STOP_WATERING = false;
+bool IS_STARTED_WATERING = false;
 int active_pin = 0;
-int pins[] = {13,23,33,32,26,27,14,15,4,25,18,19,5,21,22};
-int zone_time[] = {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1};
-String TIMER="";
+int pins[] = {23, 33, 25, 14, 26, 32, 27, 13, 22, 21, 19, 18, 5, 17, 4, 15};
+int zone_time[] = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
+String TIMER = "";
 
 TaskHandle_t Task1;
 TaskHandle_t Task2;
@@ -29,255 +33,416 @@ WiFiUDP ntpUDP;
 NTPClient timeClient(ntpUDP);
 AsyncWebServer server(80);
 
-void WRITE_VARIABLES(){
-  for(int i=0;i<PIN_COUNT;i++){
-    EEPROM.write(i,zone_time[i]);
+void WRITE_VARIABLES()
+{
+  for (int i = 0; i < PIN_COUNT; i++)
+  {
+    EEPROM.write(i, zone_time[i]);
   }
-  if(TIMER!=""){
-    EEPROM.write(PIN_COUNT,TIMER.substring(0,2).toInt());
-    EEPROM.write(PIN_COUNT+1,TIMER.substring(3,5).toInt());
+  if (TIMER != "")
+  {
+    EEPROM.write(PIN_COUNT, TIMER.substring(0, 2).toInt());
+    EEPROM.write(PIN_COUNT + 1, TIMER.substring(3, 5).toInt());
     Serial.println(EEPROM.read(PIN_COUNT));
-    Serial.println(EEPROM.read(PIN_COUNT+1));
+    Serial.println(EEPROM.read(PIN_COUNT + 1));
+  }
+  // write wifi username
+  for (int i = PIN_COUNT + 1, j = 0; i < PIN_COUNT + 22; i++, j++)
+  {
+    if (ssid[j])
+    {
+      Serial.println(String(int(ssid[j])) + " " + ssid[j] + " " + char(int(ssid[j])));
+      EEPROM.write(i, int(ssid[j]));
+    }
+    else
+    {
+      for (int q = i; q < PIN_COUNT + 22; q++)
+      {
+        EEPROM.write(q, 0);
+      }
+      break;
+    }
+  }
+  Serial.println();
+
+  // write wifi password
+  for (int i = PIN_COUNT + 21, j = 0; i < PIN_COUNT + 42; i++, j++)
+
+  {
+    if (password[j])
+    {
+      Serial.println(String(int(password[j])) + " " + password[j] + " " + char(int(password[j])));
+      EEPROM.write(i, int(password[j]));
+    }
+    else
+    {
+      for (int q = i; q < PIN_COUNT + 42; q++)
+      {
+        EEPROM.write(q, 0);
+      }
+      break;
+    }
   }
 
   EEPROM.commit();
 }
 
-void READ_VARIABLES(){
-  for(int i=0;i<PIN_COUNT;i++){
-    zone_time[i]=EEPROM.read(i);
+void READ_VARIABLES()
+{
+  // read intervals
+  for (int i = 0; i < PIN_COUNT; i++)
+  {
+    zone_time[i] = EEPROM.read(i);
   }
-    int h=EEPROM.read(PIN_COUNT);
-    int m=EEPROM.read(PIN_COUNT+1);
-    String Sh=String(h<10?"0"+String(h):h);
-    String Sm=String(m<10?"0"+String(m):m);
-    Serial.println(Sh+":"+Sm);
-    TIMER=Sh+":"+Sm;
-}
+  // read times
+  int h = EEPROM.read(PIN_COUNT);
+  int m = EEPROM.read(PIN_COUNT + 1);
+  String Sh = String(h < 10 ? "0" + String(h) : h);
+  String Sm = String(m < 10 ? "0" + String(m) : m);
+  Serial.println(Sh + ":" + Sm);
+  TIMER = Sh + ":" + Sm;
 
-void GET_INFO(AsyncWebServerRequest *request){
-  String response="{";
-  response+="\"time\":\""+TIMER+"\",";
-  response+="\"isWatering\":"+String(active_pin?"true":"false")+",";
-  response+="\"zones\":[";
-  for(int i=0;i<sizeof(pins)/sizeof(pins[0]);i++){
-    response+="{\"pin\":"+String(pins[i])+",\"interval\":"+String(zone_time[i])+",\"isEnabled\":"+(digitalRead(pins[i])==LOW?"true":"false")+"}";
-    if(i<sizeof(pins)/sizeof(pins[0])-1){
-      response+=",";
+  // read wifi ssid
+  String newSsid = "";
+  String newPassword = "";
+  for (int i = PIN_COUNT + 1; i < PIN_COUNT + 22; i++)
+  {
+    const int j = EEPROM.read(i);
+
+    if (j)
+    {
+      newSsid += char(j);
+    }
+    else
+    {
+      break;
     }
   }
-  response+="]";
-  response+="}";
+
+  // read wifi password
+  for (int i = PIN_COUNT + 21; i < PIN_COUNT + 42; i++)
+  {
+    const int j = EEPROM.read(i);
+
+    if (j)
+    {
+      newPassword += char(j);
+    }
+    else
+    {
+      break;
+    }
+  }
+  ssid = newSsid;
+  password = newPassword;
+
+  Serial.println("SSID1: " + newSsid);
+  Serial.println("Password1: " + newSsid);
+}
+
+void GET_INFO(AsyncWebServerRequest *request)
+{
+  String response = "{";
+  response += "\"time\":\"" + TIMER + "\"";
+  response += ",\"isWatering\":" + String(active_pin ? "true" : "false") + ",";
+  response += "\"zones\":[";
+  for (int i = 0; i < PIN_COUNT; i++)
+  {
+    response += "{";
+    response += "\"pin\":" + String(pins[i]);
+    response += ",\"interval\":" + String(zone_time[i]);
+    response += String(",\"isEnabled\":") + String((digitalRead(pins[i]) == LOW ? "true" : "false"));
+    response += "}";
+    if (i < sizeof(pins) / sizeof(pins[0]) - 1)
+    {
+      response += ",";
+    }
+  }
+  response += "]";
+  response += "}";
   AsyncWebServerResponse *res = request->beginResponse(200, "text/plain", response);
   res->addHeader("Access-Control-Allow-Origin", "*");
 
   request->send(res);
 }
 
-void Task1code( void * parameter) {
-  while(true){
-    start:
-    if(!active_pin){
+void Task1code(void *parameter)
+{
+  while (true)
+  {
+  start:
+    if (!active_pin)
+    {
       delay(1000);
       continue;
-    } 
-    for(int i=0; i<PIN_COUNT;i++){
-      digitalWrite(pins[i],LOW);
-      Serial.println("Start zone"+String(i));
+    }
+    for (int i = 0; i < PIN_COUNT; i++)
+    {
+      digitalWrite(pins[i], LOW);
+      Serial.println("Start zone" + String(i));
 
-      for(int j=0;j<zone_time[i]*60;j++){
-        if(active_pin==0){
+      for (int j = 0; j < zone_time[i] * 60; j++)
+      {
+        if (active_pin == 0)
+        {
           Serial.println("Success Stop");
-          digitalWrite(pins[i],HIGH);
+          digitalWrite(pins[i], HIGH);
           goto start;
         }
         delay(1000);
       }
-      digitalWrite(pins[i],HIGH);
-      active_pin=pins[i+1];
+      digitalWrite(pins[i], HIGH);
+      active_pin = pins[i + 1];
     }
-    active_pin=0;
+    active_pin = 0;
   }
 }
 
-void STOP_WATERING(AsyncWebServerRequest *request){ 
-  
- active_pin=0;
- AsyncWebServerResponse *res = request->beginResponse(200, "text/plain", "No problem, sir \n");
-    res->addHeader("Access-Control-Allow-Origin", "*");
-    request->send(res);
+void STOP_WATERING(AsyncWebServerRequest *request)
+{
+
+  active_pin = 0;
+  AsyncWebServerResponse *res = request->beginResponse(200, "text/plain", "No problem, sir \n");
+  res->addHeader("Access-Control-Allow-Origin", "*");
+  request->send(res);
 }
 
-void START_ONE(AsyncWebServerRequest *request){ 
- AsyncWebParameter* p1 = request->getParam(0);
- int pin=String(p1->value()).toInt();
- Serial.println(pin);
- if(digitalRead(pin)){
-   digitalWrite(pin,LOW);
- }else{
-   digitalWrite(pin,HIGH);
- }
-    AsyncWebServerResponse *res = request->beginResponse(200, "text/plain", "No problem, sir \n");
-    res->addHeader("Access-Control-Allow-Origin", "*");
-    request->send(res);
+void START_ONE(AsyncWebServerRequest *request)
+{
+  AsyncWebParameter *p1 = request->getParam(0);
+  int pin = String(p1->value()).toInt();
+  Serial.println(pin);
+  if (digitalRead(pin))
+  {
+    digitalWrite(pin, LOW);
+  }
+  else
+  {
+    digitalWrite(pin, HIGH);
+  }
+  AsyncWebServerResponse *res = request->beginResponse(200, "text/plain", "No problem, sir \n");
+  res->addHeader("Access-Control-Allow-Origin", "*");
+  request->send(res);
 }
 
-void SET_INTERVAL(AsyncWebServerRequest *request){
+void SET_WIFI_PASSWORD(AsyncWebServerRequest *request)
+{
+  AsyncWebParameter *p1 = request->getParam(0);
+  AsyncWebParameter *p2 = request->getParam(1);
+  AsyncWebParameter *newSsid = p1;
+  AsyncWebParameter *newPassword = p2;
+  if (p1->name() != "ssid")
+  {
+    newSsid = p2;
+    newPassword = p1;
+  }
 
- AsyncWebParameter* p1 = request->getParam(0);
- AsyncWebParameter* p2 = request->getParam(1);
- AsyncWebParameter* pin = p1;
- AsyncWebParameter* value = p2;
- if(p1->name()!="pin"){
-    pin=p2;
-    value=p1;
- }
- for(int i=0;i<PIN_COUNT;i++){
-   if(String(pins[i])==pin->value()){
-     zone_time[i]=String(value->value()).toInt();
-     Serial.print(zone_time[i]);
-     WRITE_VARIABLES();
-     break;
-   }
- }
-  
-  AsyncWebServerResponse *res = request->beginResponse(200, "text/plain","No problem, sir \n");
+  ssid = newSsid->value();
+  password = newPassword->value();
+  WRITE_VARIABLES();
+  AsyncWebServerResponse *res = request->beginResponse(200, "text/plain", "No problem, sir \n");
+  res->addHeader("Access-Control-Allow-Origin", "*");
+  request->send(res);
+}
+
+void SET_INTERVAL(AsyncWebServerRequest *request)
+{
+
+  AsyncWebParameter *p1 = request->getParam(0);
+  AsyncWebParameter *p2 = request->getParam(1);
+  AsyncWebParameter *pin = p1;
+  AsyncWebParameter *value = p2;
+  if (p1->name() != "pin")
+  {
+    pin = p2;
+    value = p1;
+  }
+  for (int i = 0; i < PIN_COUNT; i++)
+  {
+    if (String(pins[i]) == pin->value())
+    {
+      zone_time[i] = String(value->value()).toInt();
+      Serial.print(zone_time[i]);
+      WRITE_VARIABLES();
+      break;
+    }
+  }
+
+  AsyncWebServerResponse *res = request->beginResponse(200, "text/plain", "No problem, sir \n");
   res->addHeader("Access-Control-Allow-Origin", "*");
 
   request->send(res);
 }
 
-void SET_TIMER(AsyncWebServerRequest *request){
- AsyncWebParameter* p = request->getParam(0);
- TIMER=p->value();
- WRITE_VARIABLES();
- AsyncWebServerResponse *res = request->beginResponse(200, "text/plain","No problem, sir \n");
+void SET_TIMER(AsyncWebServerRequest *request)
+{
+  AsyncWebParameter *p = request->getParam(0);
+  TIMER = p->value();
+  WRITE_VARIABLES();
+  AsyncWebServerResponse *res = request->beginResponse(200, "text/plain", "No problem, sir \n");
   res->addHeader("Access-Control-Allow-Origin", "*");
   request->send(res);
 }
 
-void handleRoot(AsyncWebServerRequest *request) {
-  request->send(200, "text/html",
-  "<html>"
-  "<body>"
-  "<input placeholder='Interval' id='interval'>"
-  "<button onclick='fetch(\"/set_interval/?interval=\"+document.getElementById(\"interval\").value)'>Save Interval</button><br/>"
-  "<input placeholder='Interval' id='timer'>"
-  "<button onclick='fetch(\"/set_timer/?timer=\"+document.getElementById(\"timer\").value)'>Save Time</button><br/>"
-  "<button onclick='fetch(\"/start\")'>Start Watering</button>"
-  "<button onclick='fetch(\"/stop\")'>Stop Watering</button>"
-  "</body></html>");
+void SCAN(AsyncWebServerRequest *request)
+{
+  String json = "[";
+  int n = WiFi.scanComplete();
+  if (n == -2)
+  {
+    WiFi.scanNetworks(true);
+  }
+  else if (n)
+  {
+    for (int i = 0; i < n; ++i)
+    {
+      if (i)
+        json += ",";
+      json += "{";
+      json += "\"rssi\":" + String(WiFi.RSSI(i));
+      json += ",\"ssid\":\"" + WiFi.SSID(i) + "\"";
+      json += ",\"bssid\":\"" + WiFi.BSSIDstr(i) + "\"";
+      json += ",\"channel\":" + String(WiFi.channel(i));
+      json += ",\"secure\":" + String(WiFi.encryptionType(i));
+      json += "}";
+    }
+    WiFi.scanDelete();
+    if (WiFi.scanComplete() == -2)
+    {
+      WiFi.scanNetworks(true);
+    }
+  }
+  json += "]";
+  request->send(200, "application/json", json);
 }
 
-void START_WATERING(AsyncWebServerRequest *request){
-  active_pin=pins[0];
-  
-    AsyncWebServerResponse *res = request->beginResponse(200, "text/plain", "No problem, sir \n");
-    res->addHeader("Access-Control-Allow-Origin", "*");
-    request->send(res);
+void START_WATERING(AsyncWebServerRequest *request)
+{
+  active_pin = pins[0];
+
+  AsyncWebServerResponse *res = request->beginResponse(200, "text/plain", "No problem, sir \n");
+  res->addHeader("Access-Control-Allow-Origin", "*");
+  request->send(res);
 }
 
-void REBOOT(AsyncWebServerRequest *request){
+void REBOOT(AsyncWebServerRequest *request)
+{
   ESP.restart();
-  request->send(200, "text/plain","No problem, sir \n");
+  request->send(200, "text/plain", "No problem, sir \n");
 }
 
-void START_SERVER(void){
+void START_SERVER(void)
+{
+  Serial.println("Trying to connect to");
+  Serial.println("SSID: " + String(ssid));
+  Serial.println("Password: " + String(password));
   WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid, password);
+  WiFi.begin(ssid.c_str(), password.c_str());
   Serial.println("");
-  int i=0;
-  // Wait for connection
-  while (WiFi.status() != WL_CONNECTED) {
+  int i = 0;
+  while (WiFi.status() != WL_CONNECTED)
+  {
     delay(500);
-    if(++i>10){
-      ESP.restart();
+    if (++i > 10)
+    {
+      WiFi.softAP(HOSTssid, HOSTpassword);
+      IPAddress IP = WiFi.softAPIP();
+      Serial.println();
+      Serial.println("Started AP");
+      Serial.print(HOSTssid);
+      Serial.print(" ");
+      Serial.println(HOSTpassword);
+      Serial.print("IP address: ");
+      Serial.println(WiFi.softAPIP());
+      break;
     }
     Serial.print(".");
   }
-  Serial.println("");
-  Serial.print("Connected to ");
-  Serial.println(ssid);
-  Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());
+  if (i <= 10)
+  {
+    Serial.print("Connected to ");
+    Serial.println(ssid);
+    Serial.print("IP address: ");
+    Serial.println(WiFi.localIP());
+  }
 
-  if (MDNS.begin("esp32")) {
+  if (MDNS.begin("esp32"))
+  {
     Serial.println("MDNS responder started");
   }
-  server.on("/",HTTP_GET, handleRoot);
-  server.on("/api/start_all",HTTP_GET, START_WATERING);
-  server.on("/api/start_one",HTTP_GET, START_ONE);
-  server.on("/api/stop",HTTP_GET, STOP_WATERING);
-  server.on("/api/set_interval",HTTP_GET, SET_INTERVAL);
-  server.on("/api/set_timer",HTTP_GET, SET_TIMER);
-  server.on("/api/info",HTTP_GET, GET_INFO);
-  server.on("/api/reboot",HTTP_GET, REBOOT);
 
+  server.on("/api/start_all", HTTP_GET, START_WATERING);
+  server.on("/api/start_one", HTTP_GET, START_ONE);
+  server.on("/api/set_password", HTTP_GET, SET_WIFI_PASSWORD);
+  server.on("/api/stop", HTTP_GET, STOP_WATERING);
+  server.on("/api/set_interval", HTTP_GET, SET_INTERVAL);
+  server.on("/api/set_timer", HTTP_GET, SET_TIMER);
+  server.on("/api/info", HTTP_GET, GET_INFO);
+  server.on("/api/reboot", HTTP_GET, REBOOT);
+  server.on("/api/scan", HTTP_GET, SCAN);
 
   server.begin();
   Serial.println("HTTP server started");
-
 }
 
-void UPGRADE_BOARD(){
-   ArduinoOTA.onStart([]() {
-    Serial.println("Start");
-  });
-  ArduinoOTA.onEnd([]() {
-    Serial.println("\nEnd");
-  });
-  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
-    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
-  });
-  ArduinoOTA.onError([](ota_error_t error) {
+void UPGRADE_BOARD()
+{
+  ArduinoOTA.onStart([]()
+                     { Serial.println("Start"); });
+  ArduinoOTA.onEnd([]()
+                   { Serial.println("\nEnd"); });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total)
+                        { Serial.printf("Progress: %u%%\r", (progress / (total / 100))); });
+  ArduinoOTA.onError([](ota_error_t error)
+                     {
     Serial.printf("Error[%u]: ", error);
     if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
     else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
     else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
     else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
-    else if (error == OTA_END_ERROR) Serial.println("End Failed");
-  });
+    else if (error == OTA_END_ERROR) Serial.println("End Failed"); });
   ArduinoOTA.begin();
-  Serial.println("Ready");
-  Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());
 }
 
-void setup(void) {
+void setup(void)
+{
   EEPROM.begin(100);
-
+  delay(2000);
   Serial.begin(115200);
-  for(int i=0; i<PIN_COUNT;i++){
-    pinMode(pins[i],OUTPUT);
-    digitalWrite(pins[i],HIGH);
+  for (int i = 0; i < PIN_COUNT; i++)
+  {
+    pinMode(pins[i], OUTPUT);
+    digitalWrite(pins[i], HIGH);
   }
-  START_SERVER();
-  UPGRADE_BOARD();
-  Serial.print(EEPROM.length());
-  if(EEPROM.read(0))
+  if (EEPROM.read(0))
     READ_VARIABLES();
   else
     WRITE_VARIABLES();
-    
+
+  START_SERVER();
+  UPGRADE_BOARD();
+  Serial.print(EEPROM.length());
+
   timeClient.begin();
   timeClient.setTimeOffset(10800);
   xTaskCreatePinnedToCore(
       Task1code, /* Function to implement the task */
-      "Task2", /* Name of the task */
-      10000,  /* Stack size in words */
-      NULL,  /* Task input parameter */
-      1,  /* Priority of the task */
-      &Task2,  /* Task handle. */
-      1); /* Core where the task should run */
+      "Task2",   /* Name of the task */
+      10000,     /* Stack size in words */
+      NULL,      /* Task input parameter */
+      1,         /* Priority of the task */
+      &Task2,    /* Task handle. */
+      1);        /* Core where the task should run */
 }
 
-void loop(void) {
+void loop(void)
+{
   ArduinoOTA.handle();
   timeClient.forceUpdate();
-  Serial.print(""+timeClient.getFormattedTime()+" Timer on: ");
-  if(timeClient.getFormattedTime().indexOf(TIMER)==0&&TIMER!="" && !active_pin){
-    active_pin=pins[0];
+  // Serial.println(""+timeClient.getFormattedTime()+" Timer on: ");
+  if (timeClient.getFormattedTime().indexOf(TIMER) == 0 && TIMER != "" && !active_pin)
+  {
+    active_pin = pins[0];
   }
-  Serial.println(TIMER);
   delay(1000);
 }
